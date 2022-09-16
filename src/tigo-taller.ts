92687af1e51801f8d961ca26cce32d56eb1f9e23
Anthony Acosta M <anthony.acosta@bitsamericas.com>
2022-09-16 09:59:51 -0500

    Finished taller

--- src/tigo-taller.ts
+++ src/tigo-taller.ts
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
+import axios from 'axios';
 
 import image from '../16_9-imagen-landscape.png'
 
@@ -12,10 +13,36 @@ export class TigoTaller extends LitElement {
       width: 50%;
       place-items: center;
     }
+
+    .w-100 {
+      width: 100%;
+    }
   `;
 
+  @property({ type: Object }) person: any;
+
+  async connectedCallback() {
+    super.connectedCallback();
+    this.getProfile();
+  }
+
+  async getProfile() {
+    const { results } = (await axios.get('https://randomuser.me/api/')).data
+    const { 0: firstPerson } = results;
+    this.person = firstPerson;
+    console.log(this.person)
+  }
+
+  getImage(photo: string) {
+    return photo ? photo : image;
+  }
 
   render() {
+    const { name, picture } = this.person;
+    const { first, last } = name;
+    
+    const fullName = `${first} ${last}`;
+
     return html`
     <link rel="stylesheet" href="https://atomic.tigocloud.net/sandbox/css/main-v1.1.0.min.css">
   
@@ -23,11 +50,11 @@ export class TigoTaller extends LitElement {
       <div class="ml-general-interest">
         <div class="at-containershadow-primary">
           <div class="content-img">
-            <img src=${image} alt="Landscape" class="at-landscape-image">
+            <img src=${this.getImage(picture.large)} alt="Landscape" class="at-landscape-image w-100">
           </div>
           <div class="content-info">
-            <h5 class="at-font-h5">Llámanos gratis</h5>
-            <p class="at-font-p">a la Línea <strong>01 8000 41 51 97</strong> desde teléfonos fijos y móviles.</p>
+            <h5 class="at-font-h5">${fullName}</h5>
+            <p class="at-font-p">${this.person?.location.timezone.description}</p>
             <div class="content-button">
               <button class="at-button-tertiary">Ir al centro de ayuda</button>
             </div>
